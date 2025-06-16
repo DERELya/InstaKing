df8c83ed022ccef01c55baf4ed968e74ce153a0d
DERELya <dima.lavrenchyk85@gmail.com>
2025-06-16 17:29:45 +0300

    test

--- client/src/app/auth/login/login.component.ts
+++ client/src/app/auth/login/login.component.ts
@@ -1,11 +1,75 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {AuthService} from '../../services/auth.service';
+import {TokenStorageService} from '../../services/token-storage.service';
+import {NotificationService} from '../../services/notification.service';
+import {Router, RouterLink} from '@angular/router';
+import {MatFormField, MatInput, MatLabel} from '@angular/material/input';
+import {MatButton} from '@angular/material/button';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatLabel,
+    MatFormField,
+    MatLabel,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatFormField,
+    MatButton,
+    RouterLink,
+    HttpClientModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
+  public loginForm!: FormGroup;
+
+  constructor(
+    private authService: AuthService,
+    private tokenStorage: TokenStorageService,
+    private notificationService: NotificationService,
+    private router : Router,
+    private fb: FormBuilder
+  ) {
+    if (this.tokenStorage.getUser()){
+      this.router.navigate(['main'])
+    }
+  }
+
+  ngOnInit(): void{
+    this.loginForm=this.createLoginForm();
+  }
+
+  createLoginForm():FormGroup{
+    return this.fb.group({
+        username: ['',Validators.compose([Validators.required])],
+        password: ['',Validators.compose([Validators.required])],
+    })
+  }
+
+  submit(): void{
+    this.authService.login({
+      username: this.loginForm.value.username,
+      password: this.loginForm.value.password
+    }).subscribe(data=>{
+      console.log(data);
+      this.tokenStorage.saveToken(data.token);
+      this.tokenStorage.saveUser(data);
+
+      this.notificationService.showSnackBar('Successfully logged in');
+      this.router.navigate(['/']);
+      window.location.reload();
+
+    },error => {
+      console.log(error);
+      this.notificationService.showSnackBar(error.message)
+    })
+
+  }
 }
