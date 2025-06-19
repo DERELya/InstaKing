cf72931debab5509bc1d7e902d72c1de47941d35
DERELya <dima.lavrenchyk85@gmail.com>
2025-06-19 10:35:19 +0300

    test

--- client/src/app/user/edit-user/edit-user.component.ts
+++ client/src/app/user/edit-user/edit-user.component.ts
@@ -1,11 +1,11 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {NotificationService} from '../../services/notification.service';
-import {MAT_DIALOG_DATA, MatDialogRef, MatDialogModule} from '@angular/material/dialog';
+import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
 import {UserService} from '../../services/user.service';
 import {User} from '../../models/User';
-import {MatButton, MatButtonModule} from '@angular/material/button';
-import {MatFormField, MatInput, MatInputModule, MatLabel} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
@@ -16,57 +16,54 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     MatDialogModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDialogModule
   ],
   templateUrl: './edit-user.component.html',
   styleUrls: ['./edit-user.component.css']
 })
 export class EditUserComponent implements OnInit {
 
-  public profileEditForm!: FormGroup;
+  profileEditForm!: FormGroup;
 
   constructor(private dialogRef: MatDialogRef<EditUserComponent>,
               private fb: FormBuilder,
               private notificationService: NotificationService,
-              @Inject(MAT_DIALOG_DATA) public data:{user:User},
+              @Inject(MAT_DIALOG_DATA) public data: { user: User },
               private userService: UserService
   ) {
   }
 
   ngOnInit(): void {
-    this.profileEditForm = this.createProfileForm()
+    this.profileEditForm = this.buildProfileForm()
   }
 
-  createProfileForm(): FormGroup {
+  buildProfileForm(): FormGroup {
     return this.fb.group({
-      firstname: [
-        this.data.user.firstname,
-        Validators.compose([Validators.required])
-      ],
-      lastname: [
-        this.data.user.lastname,
-        Validators.compose([Validators.required])
-      ],
-      bio: [
-        this.data.user.bio,
-        Validators.compose([Validators.required])
-      ]
+      firstname: [this.data.user.firstname, Validators.compose([Validators.required])],
+      lastname: [this.data.user.lastname, Validators.compose([Validators.required])],
+      bio: [this.data.user.bio, Validators.compose([Validators.required])]
     })
   }
 
   submit(): void {
-    this.userService.updateUser(this.updateUser())
-      .subscribe(() => {
-        this.notificationService.showSnackBar('User udated success');
-        this.dialogRef.close();
-      })
+    if (this.profileEditForm.invalid) {
+      this.profileEditForm.markAllAsTouched();
+      return;
+    }
+    const dto = this.formToUser();
+    this.userService.updateUser(dto).subscribe({
+      next: () => {
+        this.notificationService.showSnackBar('User updated');
+        this.dialogRef.close(dto);          // вернём новые данные
+      },
+      error: () => this.notificationService.showSnackBar('Update failed')
+    });
   }
 
-  private updateUser(): User {
-    this.data.user.firstname = this.profileEditForm.value.firstname;
-    this.data.user.lastname = this.profileEditForm.value.lastname;
-    this.data.user.bio = this.profileEditForm.value.bio;
-    return this.data.user;
+  private formToUser(): User {
+    const {firstname, lastname, bio} = this.profileEditForm.value;
+    return {...this.data.user, firstname, lastname, bio};
   }
 
   closeDialog() {
