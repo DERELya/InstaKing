95434c743fc1d086562796b4501950546d7da074
DERELya <dima.lavrenchyk85@gmail.com>
2025-12-09 17:13:28 +0300

    adaptive

--- server/src/main/java/com/example/instaKing/security/SecurityConfig.java
+++ server/src/main/java/com/example/instaKing/security/SecurityConfig.java
@@ -77,7 +77,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of("http://localhost:4200","https://gratis-accompanied-seventh-anna.trycloudflare.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
